a9b4c97b52ed49e24fcf48232739c0e18dae6ab2
Srikar Maddala <maddalasrikar19@gmail.com>
2024-06-12 17:10:57 +0530

    Project report updated

--- PSO self/contours.py
+++ PSO self/contours.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 
 def f(x,y):
     'Objective function'
-    return (x-3.14)**2 + (y-2.72)**2 + np.sin(3*x+1.41) + np.sin(4*y-1.73)
+    return (x-1)**2 + (y-5)**2 + np.sin(3*x-1.41) + np.sin(4*y+1.73)
 
 
 #Computing and plotting function in 3D within [0,10]x[0,10]
